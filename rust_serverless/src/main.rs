39b5a8293ee874bc8be239ccad8f00a1f0158cb2
nabezokodaikon <nabezoko.daikon@gmail.com>
2020-03-06 19:56:35 +0900

    Add main code

--- rust_serverless/src/main.rs
+++ rust_serverless/src/main.rs
@@ -1,3 +1,31 @@
+use lambda_http::{lambda, IntoResponse, Request};
+use lambda_runtime::{error::HandlerError, Context};
+use serde_json::json;
+
+fn handler(_: Request, _: Context) -> Result<impl IntoResponse, HandlerError> {
+    Ok(json!({
+        "message": "AWS Lambda on Rust"
+    }))
+}
+
 fn main() {
-    println!("Hello, world!");
+    lambda!(handler)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handler_handles() {
+        let request: Request = Request::default();
+        let expected = json!({
+            "message": "AWS Lamnda on Rust"
+        })
+        .into_response();
+        let response = handler(request, Context::default())
+            .expect("expected Ok(_) value")
+            .into_response();
+        assert_eq!(response.body(), expected.body())
+    }
 }
